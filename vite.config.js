22ea1371e50f24e335de38be79ad4b9da574b5b4
josequemba <joseeliud33@gmail.com>
2024-06-01 02:04:14 +0100

    all

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         recipes: resolve(__dirname, "src/recipes_pages/index.html"),
+        favoritepage: resolve(__dirname, "src/favorite_page/index.html")
       },
     },
   },
